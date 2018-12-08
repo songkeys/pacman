0699cb046523dbdd923dd504a220b744bdfda850
Songkeys <zy22132@nottingham.edu.cn>
2018-12-08 13:44:58 +0000

    Add abstract class MovableGrid

---
+++ src/main/java/pacman/model/MovableGrid.java
@@ -0,0 +1,105 @@
+package pacman.model;
+
+import java.util.Set;
+import javafx.animation.AnimationTimer;
+import pacman.constant.Direction;
+import pacman.constant.MapConfig;
+
+public abstract class MovableGrid extends Grid {
+
+  public AnimationTimer moveLeft;
+  public AnimationTimer moveRight;
+  public AnimationTimer moveUp;
+  public AnimationTimer moveDown;
+
+  public MovableGrid(double x, double y) {
+    super(x, y);
+
+    // set animation
+    this.moveLeft = this.move(Direction.LEFT);
+    this.moveRight = this.move(Direction.RIGHT);
+    this.moveUp = this.move(Direction.UP);
+    this.moveDown = this.move(Direction.DOWN);
+  }
+
+  private AnimationTimer move(Direction direction) {
+    return new AnimationTimer() {
+      public void handle(long currentNanoTime) {
+        Set<Grid> obstacles = (Set<Grid>) (Set<?>) MovableGrid.this.getParentMap().getObstacles();
+
+        switch (direction) {
+          case RIGHT:
+            if (!MovableGrid.this.isTouchingGrids(Direction.RIGHT, obstacles)) {
+              MovableGrid.this.setX(MovableGrid.this.getX() + MapConfig.STEP);
+              MovableGrid.this.handleMoveRight(MovableGrid.this.getX(), MovableGrid.this.getY());
+            }
+            break;
+          case LEFT:
+            if (!MovableGrid.this.isTouchingGrids(Direction.LEFT, obstacles)) {
+              MovableGrid.this.setX(MovableGrid.this.getX() - MapConfig.STEP);
+              MovableGrid.this.handleMoveLeft(MovableGrid.this.getX(), MovableGrid.this.getY());
+            }
+            break;
+          case UP:
+            if (!MovableGrid.this.isTouchingGrids(Direction.UP, obstacles)) {
+              MovableGrid.this.setY(MovableGrid.this.getY() - MapConfig.STEP);
+              MovableGrid.this.handleMoveUp(MovableGrid.this.getX(), MovableGrid.this.getY());
+            }
+            break;
+          case DOWN:
+            if (!MovableGrid.this.isTouchingGrids(Direction.DOWN, obstacles)) {
+              MovableGrid.this.setY(MovableGrid.this.getY() + MapConfig.STEP);
+              MovableGrid.this.handleMoveDown(MovableGrid.this.getX(), MovableGrid.this.getY());
+            }
+            break;
+        }
+
+        MovableGrid.this.handleMove(MovableGrid.this.getX(), MovableGrid.this.getY());
+      }
+    };
+  }
+
+  private boolean isTouchingGrids(Direction direction, Set<Grid> grids, double padding) {
+    // generate a mock pacman at the next step
+    double nextX = this.getX();
+    double nextY = this.getY();
+    switch (direction) {
+      case RIGHT:
+        nextX += MapConfig.STEP;
+        break;
+      case LEFT:
+        nextX -= MapConfig.STEP;
+        break;
+      case UP:
+        nextY -= MapConfig.STEP;
+        break;
+      case DOWN:
+        nextY += MapConfig.STEP;
+        break;
+    }
+    Grid nextPositionGrid = new Grid(nextX / MapConfig.GRID_LENGTH, nextY / MapConfig.GRID_LENGTH);
+
+    // check if the mock pacman overlaps any obstacle
+    for (Grid grid : grids) {
+      if (grid.isTouching(nextPositionGrid, padding)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private boolean isTouchingGrids(Direction direction, Set<Grid> grids) {
+    return this.isTouchingGrids(direction, grids, 0);
+  }
+
+  public void handleMove(double x, double y) {};
+
+  public void handleMoveRight(double x, double y) {};
+
+  public void handleMoveLeft(double x, double y) {};
+
+  public void handleMoveUp(double x, double y) {};
+
+  public void handleMoveDown(double x, double y) {};
+}
--- src/main/java/pacman/model/Pacman.java
+++ src/main/java/pacman/model/Pacman.java
@@ -1,19 +1,12 @@
 package pacman.model;
 
 import java.util.Set;
-import javafx.animation.AnimationTimer;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
-import pacman.constant.Direction;
 import pacman.constant.FileName;
 import pacman.constant.MapConfig;
 
-public class Pacman extends Grid {
-
-  public AnimationTimer moveLeft;
-  public AnimationTimer moveRight;
-  public AnimationTimer moveUp;
-  public AnimationTimer moveDown;
+public class Pacman extends MovableGrid {
 
   public Pacman(double x, double y) {
     super(x, y);
@@ -21,87 +14,35 @@ public class Pacman extends Grid {
     // set image
     Image image = new Image(FileName.IMAGE_PACMAN);
     this.setFill(new ImagePattern(image));
-
-    // set animation
-    this.moveLeft = this.move(Direction.LEFT);
-    this.moveRight = this.move(Direction.RIGHT);
-    this.moveUp = this.move(Direction.UP);
-    this.moveDown = this.move(Direction.DOWN);
   }
 
-  private AnimationTimer move(Direction direction) {
-    return new AnimationTimer() {
-      public void handle(long currentNanoTime) {
-        Set<Grid> obstacles = (Set<Grid>) (Set<?>) Pacman.this.getParentMap().getObstacles();
-
-        switch (direction) {
-          case RIGHT:
-            if (!Pacman.this.isTouchingGrids(Direction.RIGHT, obstacles)) {
-              Pacman.this.setX(Pacman.this.getX() + MapConfig.STEP);
-              Pacman.this.setRotate(0);
-            }
-            break;
-          case LEFT:
-            if (!Pacman.this.isTouchingGrids(Direction.LEFT, obstacles)) {
-              Pacman.this.setX(Pacman.this.getX() - MapConfig.STEP);
-              Pacman.this.setRotate(180);
-            }
-            break;
-          case UP:
-            if (!Pacman.this.isTouchingGrids(Direction.UP, obstacles)) {
-              Pacman.this.setY(Pacman.this.getY() - MapConfig.STEP);
-              Pacman.this.setRotate(270);
-            }
-            break;
-          case DOWN:
-            if (!Pacman.this.isTouchingGrids(Direction.DOWN, obstacles)) {
-              Pacman.this.setY(Pacman.this.getY() + MapConfig.STEP);
-              Pacman.this.setRotate(90);
-            }
-            break;
-        }
-
-        Pacman.this.checkTouchingCookies();
-      }
-    };
+  @Override
+  public void handleMove(double x, double y) {
+    Pacman.this.checkTouchingCookies();
   }
 
-  private boolean isTouchingGrids(Direction direction, Set<Grid> grids, double padding) {
-    // generate a mock pacman at the next step
-    double nextX = this.getX();
-    double nextY = this.getY();
-    switch (direction) {
-      case RIGHT:
-        nextX += MapConfig.STEP;
-        break;
-      case LEFT:
-        nextX -= MapConfig.STEP;
-        break;
-      case UP:
-        nextY -= MapConfig.STEP;
-        break;
-      case DOWN:
-        nextY += MapConfig.STEP;
-        break;
-    }
-    Grid nextPacman = new Grid(nextX / MapConfig.GRID_LENGTH, nextY / MapConfig.GRID_LENGTH);
-
-    // check if the mock pacman overlaps any obstacle
-    for (Grid grid : grids) {
-      if (grid.isTouching(nextPacman, padding)) {
-        return true;
-      }
-    }
-
-    return false;
+  @Override
+  public void handleMoveRight(double x, double y) {
+    Pacman.this.setRotate(0);
   }
 
-  private boolean isTouchingGrids(Direction direction, Set<Grid> grids) {
-    return this.isTouchingGrids(direction, grids, 0);
+  @Override
+  public void handleMoveLeft(double x, double y) {
+    Pacman.this.setRotate(180);
+  }
+
+  @Override
+  public void handleMoveUp(double x, double y) {
+    Pacman.this.setRotate(270);
+  }
+
+  @Override
+  public void handleMoveDown(double x, double y) {
+    Pacman.this.setRotate(90);
   }
 
   private void checkTouchingCookies() {
-    Set<Cookie> cookies =  Pacman.this.getParentMap().getCookies();
+    Set<Cookie> cookies = Pacman.this.getParentMap().getCookies();
     for (Cookie cookie : cookies) {
       if (this.isTouching(cookie, MapConfig.COOKIE_PADDING)) {
         cookie.hide();
