69274cc16eef31e0e22d79a0c3db1de3253e36fd
Songkeys <zy22132@nottingham.edu.cn>
2018-12-13 21:08:22 +0000

    Add javadoc for Main

--- src/main/java/pacman/Main.java
+++ src/main/java/pacman/Main.java
@@ -4,20 +4,32 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import pacman.util.SceneSwitch;
 
+/** The {@link Main} class for the pacman application. */
 public class Main extends Application {
 
+  /** The primary stage of the application. */
   private static Stage primaryStage;
 
   public static void main(String[] args) {
     launch(args);
   }
 
+  /**
+   * Returns the primary stage of the application.
+   *
+   * @return the primary stage of the application
+   */
   public static Stage getPrimaryStage() {
     return primaryStage;
   }
 
+  /**
+   * Starts the application.
+   *
+   * @param primaryStage the primary stage
+   */
   @Override
-  public void start(Stage primaryStage) throws Exception {
+  public void start(Stage primaryStage) {
     Main.primaryStage = primaryStage;
 
     primaryStage.setResizable(false);
